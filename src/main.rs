e7680762f7ff53f2af910837ba192338e8d77b33
Andrew Mitchell <andrewpmitchell7@gmail.com>
2021-02-18 21:00:29 -0500

    Parse toml file into expected struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
-use anyhow::Result;
+use anyhow::{Error, Result};
 use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
 use structopt::StructOpt;
 
-fn main() {
-    let opt = Opt::from_args();
+const DEFAULT_CONFIG_DIR: &str = ".config";
+const DEFAULT_CONFIG_FILE_NAME: &str = ".surveyor.toml";
 
-    println!("{:?}", opt);
+fn main() -> Result<()> {
+    let opts = Opt::from_args();
+
+    let config_path = determine_config_path(opts.config)?;
+    let projects = get_projects_from_config_file(config_path)?;
+
+    dbg!(projects);
+    Ok(())
+}
+
+fn determine_config_path(explicit_path: Option<String>) -> Result<String> {
+    if let Some(p) = explicit_path {
+        return Ok(String::from(p));
+    }
+
+    if let Some(home) = home::home_dir() {
+        let default_file = home.join(DEFAULT_CONFIG_DIR).join(DEFAULT_CONFIG_FILE_NAME);
+        if let Some(dir) = default_file.to_str() {
+            return Ok(String::from(dir));
+        }
+    }
+
+    return Err(Error::msg("No config file specified."));
+}
+
+fn get_projects_from_config_file(file_path: String) -> Result<Vec<Project>> {
+    let mut file = File::open(file_path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let config: Config = toml::from_str(&contents)?;
+
+    Ok(config.projects)
 }
 
 #[derive(Debug, StructOpt)]
@@ -16,19 +49,23 @@ fn main() {
 struct Opt {
     /// The project to survey
     #[structopt(short, long)]
-    project: String,
+    project: Option<String>,
 
     /// The location of your config file
-    #[structopt(short, long, default_value = "~/.surveyor.toml")]
-    config: String,
+    #[structopt(
+        short,
+        long,
+        about = "Defaults to looking for a .surveyor.toml file inside of $HOME/.config/"
+    )]
+    config: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Config {
     projects: Vec<Project>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct Project {
     name: String,
     slack_webhook_url: String, // TODO: Can this be a url type?
